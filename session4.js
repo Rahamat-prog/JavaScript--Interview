6ea4e917c901e451bccd0e9babc6e0270e8f7a00
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-06-28 23:52:52 +0530

    session 4 update

---
+++ session4.js
@@ -0,0 +1,34 @@
+// const arr = [1, 2, 3];
+// console.log(arr.push(5)) // return the length of the array 
+// console.log(arr);
+
+
+// const arr = [1, 2, 3];
+// console.log(arr.unshift(5)); // return the lenght of the array 
+// console.log(arr);
+// console.log(arr.unshift());
+// console.log(arr.length);
+
+// function removeDuplicateArr(arr) {
+// const resultArr = [];
+
+// for(let i = 0; i < arr.length; i++) {
+
+//     let currentElement = arr[i];
+
+//    if(resultArr.indexOf(currentElement) === -1) {
+//      resultArr.push(currentElement);
+//    }
+       
+// }
+// return resultArr;
+// }
+
+// const result = removeDuplicateArr([1, 1, 2, 4 , 4 , 5]);
+// console.log(result);
+
+
+
+const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+console.log(words[3]0.length)
